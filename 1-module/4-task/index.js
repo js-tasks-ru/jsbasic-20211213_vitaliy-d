96c235ae725e4f1e34134a63634d5740eb5e8817
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2021-12-20 00:36:37 +0200

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  let strSpam = str.toLowerCase();
+  if (strSpam.includes('1xbet') || strSpam.includes('xxx')) {
+    return true
+  } else {
+    return false
+  }
 }
