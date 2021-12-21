9bf14aed89132c4c451f66c53e6be78fedccd475
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2021-12-21 13:58:12 +0200

    Решил все задачи

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum: function (a, b) {
+    a = this.a;
+    b = this.b
+    return a + b},
+  mul: function (a, b) {
+    a = this.a;
+    b = this.b
+    return a * b},
 };
-
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
