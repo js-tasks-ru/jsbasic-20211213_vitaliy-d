92128dabb73261bf0f3b2eeffc2d51ddaecba5d5
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2021-12-29 00:14:07 +0200

    Решил все задачи, но четвертая не проходит тест, хотя все условия задачи соблюдены

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,23 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+    .split(' ')
+    .map(function (item) {
+      if (!isNaN(item)) {
+        return +item;
+      }
+    })
+    .filter( function (item) {
+      if (item !== undefined){
+        return item;
+      }
+    })
+    .sort (function(a, b) { return a - b; })
+  let result  = {};
+  result .min = arr[0];
+  result .max = arr[arr.length - 1];
+  return result ;
 }
+
+const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
+console.log(getMinMax(inputData));
