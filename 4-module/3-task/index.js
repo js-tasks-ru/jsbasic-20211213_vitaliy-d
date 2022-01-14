ad918247de4ab52bcbaf409b1e0ebe1c36ead548
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2022-01-15 00:30:47 +0200

    Решил задачу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  let tableClass = table.tBodies[0].rows;
+  for(let tr of tableClass){
+    let elem = tr.querySelector('[data-available]');
+    if(elem !== null) {
+      let trueFalse = elem.getAttribute('data-available');
+      if (trueFalse === "true"){
+        tr.classList.add("available");
+      } else {
+        tr.classList.add("unavailable");
+      }
+    } else {
+      tr.setAttribute('hidden', '');
+    }
+    if (tr.cells[2].textContent === "m") {
+      tr.classList.add("male");
+    } else {
+      tr.classList.add("female");
+    }
+    if (tr.cells[1].textContent < 18) {
+      tr.style = "text-decoration: line-through";
+      console.log("TEST")
+    }
+  }
 }
