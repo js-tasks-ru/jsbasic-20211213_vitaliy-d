91b0e5f0cd434b70a90565f6c2d4b8cc899a7649
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2022-01-17 20:44:36 +0200

    Решил задачу

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,26 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  let carouselWidth, i = 0;
+
+  buttonLeft.style.display = 'none';
+
+  buttonRight.onclick = function () {
+      if (i < 3) {
+        carouselWidth -= carousel.offsetWidth;
+        carousel.style.transform = `translateX(${carouselWidth}px)`;
+        i++;
+        i === 3 ? buttonRight.style.display = 'none' : buttonLeft.style.display = '';
+      }
+  }
+
+  buttonLeft.onclick = function () {
+      if (i > 0) {
+        carouselWidth += carousel.offsetWidth;
+        carousel.style.transform = `translateX(${carouselWidth}px)`;
+        i--;
+        i === 0 ? buttonLeft.style.display = 'none' : buttonRight.style.display = '';
+      }
+  }
 }
